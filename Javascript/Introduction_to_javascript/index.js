2fcd38b2a1d3557f93c23a51d7c1fef39827dd7e
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-06 23:05:39 +0530

    praticising js

---
+++ Javascript/Introduction_to_javascript/index.js
@@ -0,0 +1,9 @@
+let x = 5;      // Declare x, give it the value of 5
+let y = x + 2;  // Declare y, give it the value of x + 2 
+
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+in my web page:
+*/
