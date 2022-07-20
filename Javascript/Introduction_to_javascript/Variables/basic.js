227e18fac700ae88976a79c051729b17d74f0736
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-20 17:54:31 +0530

    variables

---
+++ Javascript/Introduction_to_javascript/Variables/basic.js
@@ -0,0 +1,16 @@
+console.log("This repl contain the code for The JavaScript variable")
+
+console.log(5+8)
+// adding of Two number
+
+console.log(9-2)
+//subtraction of 2 number
+
+let a = 67 // a contains 67
+console.log(a)
+
+a = "Harry"
+console.log(a)
+//JavaScript allow this 
+//JavaScript variblie is not named as same as reserved keywords
+//Javascript is a CaseSenstive Language
