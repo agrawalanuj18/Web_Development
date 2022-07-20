e41ec42fb0955c858e2e96bd76b7795b36778f78
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-20 22:30:47 +0530

    variable_constant

---
+++ Javascript/Introduction_to_javascript/Variables/Primitive_constant/index.js
@@ -0,0 +1,27 @@
+// NN BB  SS U
+// it's way to learn data types
+// Primitve Data Types
+
+let a = null;
+let b = 18;
+let c = true;
+let d = BigInt("5674");
+let e = "Hello";
+let f = Symbol("This bis a Symbol");
+let g = undefined;
+
+console.log(a, b, c, d, e, f, g);
+console.log(typeof d);
+
+// Objects
+// It is not Primmitve data types
+// Ishe apan mapping baana sakte h
+
+const item = {
+  hello: null,
+  ing: 18,
+  j: true,
+  k: false,
+};
+console.log(item["hello"]);
+console.log(item["k"]);
