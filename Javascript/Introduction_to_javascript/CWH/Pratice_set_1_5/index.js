570ab12badfde7df2621bd6f64e861f650ff0aba
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-21 12:10:17 +0530

    delete unnecessary file

--- Javascript/Introduction_to_javascript/CWH/Pratice_set_1_5/index.js
+++ Javascript/Introduction_to_javascript/CWH/Pratice_set_1_5/index.js
@@ -1,18 +1,18 @@
 // Chapter 1
 //****Question1****
 // Create a variable of Type String
-let a = "Add"
-console.log(a + " " + 5)
+let a = "Add";
+console.log(a + " " + 5);
 
 // ****Question 2****
 // find the datatype of a using typeof
 
-console.log(typeof a)
+console.log(typeof a);
 
 // ****Queston3****
 
 // Create a constant object
-const b = 55
+const b = 55;
 // It cannot change in the code
 // But we can change internally in objects
 
@@ -23,24 +23,24 @@ const b = 55
 const c = {
   name: "Akku",
   section: 1,
-  isPrincipal: false
-}
+  isPrincipal: false,
+};
 
-c['friend'] = "Harry"
-c['name'] = "Aman"
+c["friend"] = "Harry";
+c["name"] = "Aman";
 
-console.log(c)
-// toh apan object ke aandar change kar sakte h 
-// but object ko alag nhi kar sakte h 
+console.log(c);
+// toh apan object ke aandar change kar sakte h
+// but object ko alag nhi kar sakte h
 
 // ****Question5****
-// Create a dictionary of 5 words 
+// Create a dictionary of 5 words
 const dictonary = {
   Hello: "Namaste",
   Number: "Digit",
   Byy: "Leave",
   Create: "Make",
   Question: "Probelm",
-}
-console.log(dictonary["Hello"])
-console.log(dictonary.Byy)
+};
+console.log(dictonary["Hello"]);
+console.log(dictonary.Byy);
--- Javascript/Introduction_to_javascript/CWH/Variables/index.js
+++ Javascript/Introduction_to_javascript/CWH/Variables/index.js
@@ -1,16 +1,16 @@
-console.log("This repl contain the code for The JavaScript variable")
+console.log("This repl contain the code for The JavaScript variable");
 
-console.log(5+8)
+console.log(5 + 8);
 // adding of Two number
 
-console.log(9-2)
+console.log(9 - 2);
 //subtraction of 2 number
 
-let a = 67 // a contains 67
-console.log(a)
+let a = 67; // a contains 67
+console.log(a);
 
-a = "Harry"
-console.log(a)
-//JavaScript allow this 
+a = "Harry";
+console.log(a);
+//JavaScript allow this
 //JavaScript variblie is not named as same as reserved keywords
 //Javascript is a CaseSenstive Language
