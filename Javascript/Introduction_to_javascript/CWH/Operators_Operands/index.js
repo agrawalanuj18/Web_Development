54fa50152296132ebffba47e44dc04809a76a0d5
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-24 22:45:34 +0530

    neat code

--- Javascript/Introduction_to_javascript/CWH/Operators_Operands/index.js
+++ Javascript/Introduction_to_javascript/CWH/Operators_Operands/index.js
@@ -1,47 +1,48 @@
-console.log("Operators in Js")
+console.log("Operators in Js");
 let a = 45;
 let b = 4;
-console.log("Various Types of operator")
-console.log("a + b = ", a + b)
-console.log("a - b = ", a - b)
-console.log("a / b = ", a / b)
-console.log("a * b = ", a * b)
-console.log("a ** b = ", a ** b) //  a to power b
-console.log("a % b = ", a % b) // gives remainder
+console.log("Various Types of operator");
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a / b = ", a / b);
+console.log("a * b = ", a * b);
+console.log("a ** b = ", a ** b); //  a to power b
+console.log("a % b = ", a % b); // gives remainder
 
-console.log("Increment Operator")
-console.log("a++ = ", a++)
-console.log("value of a", a)
-console.log("++a = ", ++a)
-console.log("value of a", a)
+console.log("Increment Operator");
+console.log("a++ = ", a++);
+console.log("value of a", a);
+console.log("++a = ", ++a);
+console.log("value of a", a);
 
-console.log("Decrement Operator")
-console.log("a-- =", a--)
-console.log("value of a", a)
-console.log("--a = ", --a)
-console.log("value of a", a)
+console.log("Decrement Operator");
+console.log("a-- =", a--);
+console.log("value of a", a);
+console.log("--a = ", --a);
+console.log("value of a", a);
 
-// Assignment Operator
+
+// ***** Assignment Operator*******
 
 let c = 50;
-c += 5
-console.log("c+=5", c) // c = c+5
-c -= 5
-console.log("c-=5", c) // c = c-5
-c *= 5
-console.log("c*=5", c) // c = c*5
-c /= 5
-console.log("c/=5", c) // c = c/5
+c += 5;
+console.log("c+=5", c); // c = c+5
+c -= 5;
+console.log("c-=5", c); // c = c-5
+c *= 5;
+console.log("c*=5", c); // c = c*5
+c /= 5;
+console.log("c/=5", c); // c = c/5
 
-// Comparsion Operator
-let comp1 = 6
-let comp2 = "6"
-console.log("comp1 == comp2", comp1 == comp2)
-console.log("comp1 != comp2", comp1 != comp2)
-console.log("comp1 === comp2", comp1 === comp2)
-console.log("comp1 !== comp2", comp1 !== comp2)
+// *********Comparsion Operator***********
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1 == comp2", comp1 == comp2);
+console.log("comp1 != comp2", comp1 != comp2);
+console.log("comp1 === comp2", comp1 === comp2);
+console.log("comp1 !== comp2", comp1 !== comp2);
 
-// This is Single Line Comment 
+// This is Single Line Comment
 /*
   this is
  multi line
