621f9ac25825bb8efd80a23a045215fbb7710c08
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-29 14:28:21 +0530

    Start Chapter 2 JS

---
+++ Javascript/CWH/Pratice_setChap2/q1.js
@@ -0,0 +1,11 @@
+console.log("Hello to Pratice Set 2")
+// question 1
+//wheather the age of person between 10 and 20 or not
+let age = prompt("Enter the age");
+age = Number.parseInt(age);
+if (age>10 && age <20){
+  alert("Age is between 10 and 20")
+}
+else {
+  alert("Age is not lie between 10 and 20")
+}
