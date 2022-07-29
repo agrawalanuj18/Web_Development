00852f06179a95e04a7ccbaea6560f568fb724fb
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-29 16:07:33 +0530

    question 3

---
+++ Javascript/CWH/Pratice_setChap2/q3.js
@@ -0,0 +1,10 @@
+// Question 3 
+// Find the wheather a number is divisble by 2 and 3
+let num = prompt("Enter the number");
+num = Number.parseInt(num);
+if(num%2==0 && num%3==00){
+  console.log("a number is divisble by 2 and 3")
+}
+else{
+  console.log("a number is not divisble by 2 and 3")
+}
