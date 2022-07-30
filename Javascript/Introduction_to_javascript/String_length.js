e14d187812696e659b66cbc5350400b3605471f1
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-31 03:07:44 +0530

    string

---
+++ Javascript/Introduction_to_javascript/String_length.js
@@ -0,0 +1,4 @@
+let character = prompt("Enter the character")
+let count = character.length;
+let remain = 220 - count;
+console.log("You have written " + count + " character and you have " + remain + " character left")
