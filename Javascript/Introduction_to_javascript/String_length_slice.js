b10c4b2d91669bffed2d4a86a41f558a78984b92
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-31 11:49:22 +0530

    slice string

--- Javascript/Introduction_to_javascript/String_length_slice.js
+++ Javascript/Introduction_to_javascript/String_length_slice.js
@@ -2,3 +2,4 @@ let character = prompt("Enter the character")
 let count = character.length;
 let remain = 220 - count;
 console.log("You have written " + count + " character and you have " + remain + " character left")
+alert(character.slice(0,220))
