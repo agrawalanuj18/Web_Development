e15afd6e3fdd1dbf36a0868b07a62baa41cfc9dd
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-01 15:17:10 +0530

    Chapter 3 pratice set solving

---
+++ Javascript/CWH/Pratice_setChap3/q1.js
@@ -0,0 +1,12 @@
+// Chapter 3 Pratice Set
+// Loops and Function 
+
+// Question 1 
+// Write a program to print the marks of student.....
+let obj = {
+    harry : 98,
+    rohan : 70,
+    Aakhas : 7,
+  }
+  console.log(obj["1"])
+ 
