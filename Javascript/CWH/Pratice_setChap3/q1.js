55fc8e4efd29feb76c381644028ed06e8e5277cb
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-02 01:34:11 +0530

    q1

--- Javascript/CWH/Pratice_setChap3/q1.js
+++ Javascript/CWH/Pratice_setChap3/q1.js
@@ -3,10 +3,12 @@
 
 // Question 1 
 // Write a program to print the marks of student.....
-let obj = {
+let marks = {
     harry : 98,
     rohan : 70,
     Aakhas : 7,
   }
-  console.log(obj["1"])
- 
+  
+  for(let i = 0; i < Object.keys(marks).length; i++){
+    console.log("The marks of " + Object.keys(marks)[i] + "are " + marks[Object.keys(marks)[i]])
+  }
