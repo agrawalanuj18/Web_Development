5f1b611956720165ba494183df834efef74e5416
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-03 21:22:32 +0530

    Start DOM

---
+++ Javascript/CWH/String/stringFunction.js
@@ -0,0 +1,16 @@
+let name = "Harry";
+console.log(name);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name[0]);
+console.log(name[1]);
+console.log(name.slice(2,4))
+console.log(name.slice(2))
+console.log(name.replace("ry", "py"))
+console.log(name);
+let friend ="aman"
+console.log(name.concat(" is a friend of ", friend ));
+let friend2 ="     aman   "
+console.log(friend2);
+console.log(friend2.trim())
