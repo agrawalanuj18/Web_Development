cf4237c4a55a9ec758c7640b6a4392804f7c6bec
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-18 23:58:06 +0530

    hello

---
+++ Javascript/CWH/01_PraticeSet/Pratice-setChap5/q1.js
@@ -0,0 +1,6 @@
+// Question 1 Pratice Set
+let arr = [1,2,3,4,5,6];
+let a = prompt("Enter the number");
+a = Number.parseInt(a);
+arr.push(a)
+console.log(arr)
---
+++ Javascript/CWH/01_PraticeSet/Pratice-setChap5/q5.js
@@ -0,0 +1,6 @@
+// // // // Question 4 Pratice Set
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let c = arr.reduce((h1, h2)=>{
+  return h1 * h2;
+})
+console.log(c)
