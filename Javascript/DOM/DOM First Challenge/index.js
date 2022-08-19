d6eedf14572725e0967ad55b655e9afbef722681
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-19 20:39:09 +0530

    lecture 144

--- Javascript/DOM/DOM First Challenge/index.js
+++ Javascript/DOM/DOM First Challenge/index.js
@@ -1,1 +1,4 @@
-document.firstElementChild.lastElementChild.firstElementChild.lastElementChild.innerHTML = "Angela";
+// document.firstElementChild.lastElementChild.firstElementChild.lastElementChild.innerHTML = "Angela";
+document.querySelector("li a").style.color = "red"
+document.querySelector("h1").style.color = "blue"
+document.querySelector("button").style.backgroundColor = "yellow"
