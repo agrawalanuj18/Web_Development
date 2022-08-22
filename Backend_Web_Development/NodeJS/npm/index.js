1e0442d25f84b7364878ab1732e95cedf7f732d6
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-22 17:49:50 +0530

    NodeJS

---
+++ Backend_Web_Development/NodeJS/npm/index.js
@@ -0,0 +1,10 @@
+let superheroes = require('superheroes');
+
+let superHeroName = superheroes.random();
+
+let supervillains = require('supervillains');
+
+let superVillianName = supervillains.random();
+
+
+console.log(superHeroName + " vs " + superVillianName);
