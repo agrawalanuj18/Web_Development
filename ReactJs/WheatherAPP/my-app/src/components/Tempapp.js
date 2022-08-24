1d705db7b15f1ff7f322a704323dab8094706043
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-24 22:52:36 +0530

    Wheather React JS

--- ReactJs/WheatherAPP/my-app/src/components/Tempapp.js
+++ ReactJs/WheatherAPP/my-app/src/components/Tempapp.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style/style.css";
 
  const Tempapp = () => {
 
-    const [city, setCity] = useState(null)
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("Mumbai");
 
+    useEffect( () => {
+        
+        const fetchApi = async() => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=147389ae0f1c5ba915bc4f7cdf5819f7`;
+            const response = await fetch(url);
+            const resJson = await response.json();
+            setCity(resJson.main);
+            
+        };
 
+        fetchApi();
+
+    }, [search]  )
+     
 
     return(<>
     
@@ -13,30 +27,33 @@ import "./style/style.css";
 
     <div className="inputData">
 
-    <input type="search" className="inputFeild" onChange={
-        (event) => {
-
+    <input type="search" value= {search}  className="inputFeild" 
+    onChange={ (event) => {
+            setSearch(event.target.value)
         }
     } />
-
     </div>
 
+{!city ? (
+        <p className="errorMsg"> No Data Found</p>
+    ) : (
+        <div>
+        <div className="info">
 
-    <div className="info">
-
-    <h2 className="location"> <i class="fa-solid fa-street-view"></i>  Pune </h2>
-   <h1 className="temp"> 
-   
-   5.25°Cel
-   
-   </h1>
-   <h3 className="tempmin_max"> Min : 5.25°Cel | Max : 5.25°Cel  </h3>
-
+        <h2 className="location"> 
+        <i className="fa-solid fa-street-view">
+            </i>  {search} </h2>
+       <h1 className="temp"> {city.temp}Cel </h1>
+       <h3 className="tempmin_max"> Min : {city.temp_min} | Max : {city.temp_max}  </h3>
+    
+        </div>
+    
+        <div className="wave -one"></div>
+        <div className="wave -two"></div>
+        <div className="wave -three "></div>
     </div>
-
-    <div className="wave -one"></div>
-    <div className="wave -two"></div>
-    <div className="wave -three "></div>
+    )
+}
 
 
     </div>
