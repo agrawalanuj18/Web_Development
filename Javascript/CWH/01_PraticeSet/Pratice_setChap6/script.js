bd0b0e14da215a4a0a29338cf1e3feb5c53941d3
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-29 03:28:15 +0530

    Chapter 6 Pratice set

---
+++ Javascript/CWH/01_PraticeSet/Pratice_setChap6/script.js
@@ -0,0 +1,55 @@
+// Question 1 
+
+// let age = prompt("Enter your age");
+// Number.parseFloat(age)
+// if(age >= 18){
+//   alert("You can Drive")
+// }
+// else {
+//   alert("You cannot Drive")
+// }
+
+// Question-2
+
+// do {
+//   let age = prompt("Enter your age");
+//   Number.parseFloat(age)
+//   if (age >= 18) {
+//     alert("You can Drive")
+//   }
+//   else {
+//     alert("You cannot Drive")
+//   }
+// } while (confirm("You want to see it again") == true)
+
+// Question 3
+
+// do {
+//   let age = prompt("Enter your age");
+//   Number.parseFloat(age)
+//   if (age < 0) {
+//     console.error(age < 0)
+//     break;
+//   }
+
+//   if (age >= 18) {
+//     alert("You can Drive")
+//   }
+//   else {
+//     alert("You cannot Drive")
+//   }
+// } while (confirm("You want to see it again") == true)
+
+// Question-4
+
+// let  num = prompt("Enter your Number");
+// Number.parseFloat(num)
+// if(num > 4 ){
+//   window.location.href="https://www.google.com/"
+// }
+
+// Question-5
+
+// let color = prompt("Enter your color")
+
+// document.body.style.backgroundColor = color
