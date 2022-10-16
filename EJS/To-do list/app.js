6ed383511d100decb1169cfc3d5b541bb1258f8f
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-16 14:36:44 +0530

    EJS

--- EJS/To-do list/app.js
+++ EJS/To-do list/app.js
@@ -1,13 +1,43 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
-const port = 3000
+const app = express();
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html") 
-})
+const port = 3000;
+
+app.get("/", (req, res) => {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+  }
+
+  res.render("list", { KindOfDay: day });
+});
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
-})
+  console.log(`Server started on port ${port}`);
+});
