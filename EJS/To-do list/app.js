7416f474aa7191bcb30a817a3f911fa80d3b9751
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-17 15:22:25 +0530

    EJS

--- EJS/To-do list/app.js
+++ EJS/To-do list/app.js
@@ -2,41 +2,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Learn Web Development", "DSA"];
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 const port = 3000;
 
 app.get("/", (req, res) => {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  
+  var option = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-  }
+  var day = today.toLocaleDateString("en-US", option)
 
-  res.render("list", { KindOfDay: day });
+  res.render("list", { KindOfDay: day, newListItem: items });
 });
+
+app.get("/work", ()=>{
+  
+})
+
+app.post("/", (req, res)=>{
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
+
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
