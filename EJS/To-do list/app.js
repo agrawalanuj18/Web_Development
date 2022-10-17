ef54ab7d36b81010ec873c9d310141ed7b867961
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-17 23:13:25 +0530

    EJS

--- EJS/To-do list/app.js
+++ EJS/To-do list/app.js
@@ -5,35 +5,32 @@ const app = express();
 var items = ["Learn Web Development", "DSA"];
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const port = 3000;
 
 app.get("/", (req, res) => {
   var today = new Date();
-  
+
   var option = {
     weekday: "long",
     day: "numeric",
-    month: "long"
+    month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", option)
+  var day = today.toLocaleDateString("en-US", option);
 
   res.render("list", { KindOfDay: day, newListItem: items });
 });
 
-app.get("/work", ()=>{
-  
-})
+app.get("/work", () => {});
 
-app.post("/", (req, res)=>{
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+app.post("/", (req, res) => {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
-
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
