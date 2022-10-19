2f50681c7915a1a374b6becf9e4ca192673ff0f8
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-19 23:23:24 +0530

    add

--- EJS/To-do list/app.js
+++ EJS/To-do list/app.js
@@ -3,23 +3,24 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const app = express();
 let items = [];
-let workItems = []; 
+let workItems = [];
 // In const, we can push the value of Array but not able to assign the value
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 const port = 3000;
 
 app.get("/", (req, res) => {
   let day = date.getDate();
   res.render("list", { listTitle: day, newListItem: items });
 });
-
 app.get("/work", (req, res) => {
   let day = date.getDay();
-  res.render("list", { listTitle: "Work List, " + day, newListItem: workItems });
+  res.render("list", {
+    listTitle: "Work List, " + day,
+    newListItem: workItems,
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
@@ -36,8 +37,6 @@ app.post("/", (req, res) => {
     res.redirect("/");
   }
 });
-
-
 
 app.post("/work", (req, res) => {
   let item = req.body.newItem;
